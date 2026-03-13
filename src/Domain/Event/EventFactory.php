57651ce047e36b74a117400d00dbc8805e0b6aff
Piotr Biernat <keedosn@gmail.com>
2026-03-13 17:40:00 +0100

    Added createValidator method in EventFactory class

--- src/Domain/Event/EventFactory.php
+++ src/Domain/Event/EventFactory.php
@@ -3,18 +3,23 @@ declare(strict_types=1);
 
 namespace App\Domain\Event;
 
+use App\Domain\Event\Type\Event;
 use App\Domain\Event\Type\FoulEvent;
 use App\Domain\Event\Type\GoalEvent;
+use App\Domain\Event\Validator\BaseEventValidator;
+use App\Domain\Event\Validator\FoulEventDecorator;
+use App\Domain\Event\Validator\GoalEventDecorator;
+use App\Domain\Event\Validator\ValidatorInterface;
 
 class EventFactory
 {
-    public static function fromArray(array $data)
+    public static function fromArray(array $data): Event
     {
-        $eventClassName = sprintf('App\Domain\Event\Type\%sEvent', ucfirst($data['type']));
-
-        if (!class_exists($eventClassName)) {
-            throw new \InvalidArgumentException('Unknown EventType class: '. $eventClassName);
+        if (!isset($data['type'])) {
+            throw new \InvalidArgumentException('Missing type property');
         }
+
+        $eventClassName = static::createEventTypeClassName($data['type']);
 
         switch ($eventClassName) {
             case GoalEvent::class:
@@ -26,7 +31,7 @@ class EventFactory
                     $data['minute'],
                     $data['second']
                 );
-            case FoulEvent::class:
+            default:
                 return new FoulEvent(
                     $data['player'],
                     $data['affected_player'],
@@ -36,5 +41,29 @@ class EventFactory
                     $data['second']
                 );
         }
+    }
+
+    public static function createValidator(string $type): ValidatorInterface
+    {
+        $eventClassName = static::createEventTypeClassName($type);
+        $validator = new BaseEventValidator();
+
+        $validator = match ($eventClassName) {
+            GoalEvent::class => new GoalEventDecorator($validator),
+            FoulEvent::class => new FoulEventDecorator($validator),
+        };
+
+        return $validator;
+    }
+
+    private static function createEventTypeClassName(string $type): string
+    {
+        $eventClassName = sprintf('App\Domain\Event\Type\%sEvent', ucfirst($type));
+
+        if (!class_exists($eventClassName)) {
+            throw new \InvalidArgumentException('Unknown EventType class: '. $eventClassName);
+        }
+
+        return $eventClassName;
     }
 }
